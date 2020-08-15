2062d6e3eaed463b33b48c938e64887fff89acda
shonalishinde <66588622+shonalishinde@users.noreply.github.com>
2020-08-15 13:27:26 +0530

    Update contact.php

--- Portfolio Website/contact.php
+++ Portfolio Website/contact.php
@@ -1,9 +1,9 @@
 <?php
 
-$name = $_GET['name'];
-$email = $_GET['email'];
-$subject = $_GET['subject'];
-$message = $_GET['message'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
 if( empty($name) || empty($email) || empty($subject) || empty($message) ) {
     echo "Please the all details";
@@ -16,11 +16,9 @@ else {
                   "Email: $email_address\n Message : $message \n";
     $headers = "From : Portfolio";
 
-    if ( mail($to, $email_sub, $email_body, $headers) )
-    {
+    mail($to, $email_sub, $email_body, $headers);
+    
     echo "<script type='text/javascript'> alert('Your Message has been successfully sent to Sonali. Thank you!'); window.history.log(-1); </script>";
-    }
-    else
-        echo "<script type='text/javascript'> alert('Cannot send'); window.history.log(-1); </script>";
+    
 }
 ?>
